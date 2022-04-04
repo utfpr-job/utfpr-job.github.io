29e74e642573f1d004259dd22bbae04d5e9486d2
Gabriel Carneiro <gabriel.carneiro@linkapi.com.br>
2022-04-04 19:18:43 -0300

    feat: add reaload graph button and make it work

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const dataSet = []
-for (let i = 0; i < 70; i ++) {
+let dataSet = []
+
+for (let i = 0; i < 50; i ++) {
   dataSet.push(Math.floor(Math.random() * 100))
 }
 
-const mkBar = (size) => `<div class="bar" style="height: ${size * 2}px; margin-top: ${140 - size * 2}px;"></div>`
-
-for (let i = 0; i < dataSet.length; i++) {
-  $('.target').append(mkBar(dataSet[i]));
-}
 
 function resetBackgroud() {
   $('.bar').css('background', '#5050F5')
@@ -17,11 +13,35 @@ function setActualPosition(position) {
   $('.bar').eq(position).css('background', 'red')
 }
 
+let mkBar
+
+function bar(){
+  document.querySelector('.target').innerHTML = ''
+
+  mkBar = (size) => `<div class="bar" style="height: ${size * 2}px; margin-top: ${140 - size * 2}px;"></div>`
+  
+  for (let i = 0; i < dataSet.length; i++) {
+    $('.target').append(mkBar(dataSet[i]));
+  }
+  clearTimeout(timeout)
+  search(Math.floor(Math.random() * 50))
+}
+
+document.querySelector('button').addEventListener('click', () => {
+  dataSet = []
+  for (let i = 0; i < 50; i ++) {
+    dataSet.push(Math.floor(Math.random() * 100))
+  }
+  bar()})
+ 
+let timeout
+
 function search(position) {
+  
   resetBackgroud()
   setActualPosition(position)
-  
-  setTimeout(function () {
+
+  timeout = setTimeout(function () {
     if (dataSet?.[position + 1] > dataSet[position]) {
       position++
       search(position)
@@ -34,7 +54,7 @@ function search(position) {
       const random = Math.floor(Math.random() * dataSet.length - 1) + 1
       search(random)
     }
-  }, 500)
-}
+    }, 500)
+  }
 
-search(Math.floor(Math.random() * 70))
+bar()
