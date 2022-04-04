7b34c3c8265f22b2e8d4ca70901a7d6f830c972f
filiafobico <oliveira1eduardo2luis3@gmail.com>
2022-04-04 20:16:08 -0300

    refactor: padronize code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let dataSet = []
-
-for (let i = 0; i < 50; i ++) {
-  dataSet.push(Math.floor(Math.random() * 100))
+function createRandomData (length = 50) {
+  return Array.from({ length }).map(() => Math.floor(Math.random() * 100))
 }
-
 
 function resetBackgroud() {
   $('.bar').css('background', '#5050F5')
@@ -13,48 +10,45 @@ function setActualPosition(position) {
   $('.bar').eq(position).css('background', 'red')
 }
 
-let mkBar
+function mkBar (size) {
+  return `<div class="bar" style="height: ${size * 2}px; margin-top: ${140 - size * 2}px;"></div>`
+}
 
-function bar(){
+function bar(data) {
   document.querySelector('.target').innerHTML = ''
 
-  mkBar = (size) => `<div class="bar" style="height: ${size * 2}px; margin-top: ${140 - size * 2}px;"></div>`
-  
-  for (let i = 0; i < dataSet.length; i++) {
-    $('.target').append(mkBar(dataSet[i]));
+  for (let i = 0; i < data.length; i++) {
+    $('.target').append(mkBar(data[i]));
   }
-  clearTimeout(timeout)
-  search(Math.floor(Math.random() * 50))
+  search(data, Math.floor(Math.random() * 50))
 }
 
 document.querySelector('button').addEventListener('click', () => {
-  dataSet = []
-  for (let i = 0; i < 50; i ++) {
-    dataSet.push(Math.floor(Math.random() * 100))
-  }
-  bar()})
- 
-let timeout
+  start()
+})
 
-function search(position) {
-  
+function search(data, position) {
   resetBackgroud()
   setActualPosition(position)
 
-  timeout = setTimeout(function () {
-    if (dataSet?.[position + 1] > dataSet[position]) {
+  setTimeout(function () {
+    if (data?.[position + 1] > data[position]) {
       position++
-      search(position)
+      search(data, position)
     }
-    else if (dataSet?.[position - 1] > dataSet[position]) {
+    else if (data?.[position - 1] > data[position]) {
       position--
-      search(position)
+      search(data, position)
     }
     else {
-      const random = Math.floor(Math.random() * dataSet.length - 1) + 1
-      search(random)
+      const random = Math.floor(Math.random() * data.length - 1) + 1
+      search(data, random)
     }
-    }, 500)
-  }
+  }, 1000)
+}
 
-bar()
+function start () {
+  bar(createRandomData())
+}
+
+start()
